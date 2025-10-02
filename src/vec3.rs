e6e17960e79f844f3c8f59f412af325866c63ddd
Hadrian Hughes <hadrianhughes@gmail.com>
2025-10-02 18:14:15 +0100

    Define a Vec3 type with Point3 and Color aliases

---
+++ src/vec3.rs
@@ -0,0 +1,23 @@
+#[derive(Copy, Clone, Default)]
+pub struct Vec3(f64, f64, f64);
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3(x, y, z)
+    }
+
+    pub fn x(&self) -> f64 {
+        self.0
+    }
+
+    pub fn y(&self) -> f64 {
+        self.1
+    }
+
+    pub fn z(&self) -> f64 {
+        self.2
+    }
+}
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
