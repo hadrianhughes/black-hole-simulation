c9aed958eebd67463329442ec8121d37f6a59b1a
Hadrian Hughes <hadrianhughes@gmail.com>
2025-10-04 18:23:20 +0100

    Create a Ray struct type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod vec3;
 mod color;
+mod ray;
 
 use std::io;
 
---
+++ src/ray.rs
@@ -0,0 +1,28 @@
+use crate::vec3::{Point3, Vec3};
+
+#[derive(Default)]
+pub struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Ray {
+            origin,
+            direction,
+        }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.direction
+    }
+
+    pub fn at(&self, t: f64) -> f64 {
+        self.origin + self.direction * t
+    }
+}
