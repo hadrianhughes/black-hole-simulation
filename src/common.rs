65e9684f53989ac19f8f7f7e8cdbcd047f5433d4
Hadrian Hughes <hadrianhughes@gmail.com>
2025-10-06 20:16:58 +0100

    Implement antialiasing using supersampling

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,13 @@
-use rand::Rng;
-
 pub fn in_range(min: f64, x: f64, max: f64) -> bool {
     x > min && x < max
 }
 
-pub fn random_in_range(min: f64, max: f64) -> f64 {
-    min + (max - min) * rand::rng().random::<f64>()
+pub fn clamp(min: f64, x: f64, max: f64) -> f64 {
+    if x < min {
+        return min;
+    }
+    if x > max {
+        return max;
+    }
+    x
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod sphere;
 mod vec3;
 
 use std::io;
+use rand::Rng;
 
 use camera::Camera;
 use color::Color;
@@ -29,6 +30,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+    const SAMPLES_PER_PIXEL: i32 = 100;
 
     let mut world = HittableList::new();
     world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
@@ -39,15 +41,20 @@ fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {}", j);
+
         for i in 0..IMAGE_WIDTH {
-            eprint!("\rScanlines remaining: {}", j);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
 
-            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let r = camera.get_ray(u, v);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + rand::rng().random::<f64>()) / (IMAGE_WIDTH - 1) as f64;
+                let v = (j as f64 + rand::rng().random::<f64>()) / (IMAGE_HEIGHT - 1) as f64;
+                let r = camera.get_ray(u, v);
 
-            let pixel_color = ray_color(&r, &world);
-            color::write_color(&mut io::stdout(), pixel_color);
+                pixel_color += ray_color(&r, &world);
+            }
+
+            color::write_color(&mut io::stdout(), pixel_color, SAMPLES_PER_PIXEL);
         }
     }
 
