2dc373dca22fd60e45b574cf24d56d64701de54d
Hadrian Hughes <hadrianhughes@gmail.com>
2025-10-06 21:26:00 +0100

    Reduce number of samples per pixel to 50

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-    const SAMPLES_PER_PIXEL: i32 = 100;
+    const SAMPLES_PER_PIXEL: i32 = 50;
     const MAX_DEPTH: i32 = 50;
 
     let world = HittableList::new()
